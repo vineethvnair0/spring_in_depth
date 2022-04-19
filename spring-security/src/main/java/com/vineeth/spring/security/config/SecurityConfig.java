c01618df70c027468bc5fdb33304c0d270750688
vineethvnair0 <vineethvnair0@gmail.com>
2022-04-19 22:47:49 +0530

    fixing the security issue

--- spring-security/src/main/java/com/vineeth/spring/security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/vineeth/spring/security/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/","/api/*").permitAll()
+        http.authorizeRequests()
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
                 .and().logout().permitAll();
     }
