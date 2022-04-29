c0be692ff826c3c5d3f834e2afbc52118385434c
vineethvnair0 <vineethvnair0@gmail.com>
2022-04-29 23:07:35 +0530

    finishing the project with tour rating

---
+++ spring-boot-microservice/src/main/java/com/vineeth/learning/spring/microservice/model/TourRating.java
@@ -0,0 +1,76 @@
+package com.vineeth.learning.spring.microservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+public class TourRating {
+
+    @EmbeddedId
+    private TourRatingPk pk;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    @Column
+    private String comment;
+
+    public TourRating() {
+    }
+
+    public TourRating(TourRatingPk pk, Integer score, String comment) {
+        this.pk = pk;
+        this.score = score;
+        this.comment = comment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TourRating that = (TourRating) o;
+        return Objects.equals(pk, that.pk) && Objects.equals(score, that.score) && Objects.equals(comment, that.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pk, score, comment);
+    }
+
+
+    @Override
+    public String toString() {
+        return "TourRating{" +
+                "tourRatingPk=" + pk +
+                ", score=" + score +
+                ", cpmment='" + comment + '\'' +
+                '}';
+    }
+
+
+    public TourRatingPk getTourRatingPk() {
+        return pk;
+    }
+
+    public void setTourRatingPk(TourRatingPk pk) {
+        this.pk = pk;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
